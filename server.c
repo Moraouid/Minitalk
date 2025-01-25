ef0bf5d038e0da3f83b2f3d1d1c0c67937e779db
Soufiane El Abboubi <sel-abbo@c2r4p5.1337.ma>
2025-01-25 09:59:19 +0100

    1337

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: sel-abbo <sel-abbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 18:33:47 by sel-abbo          #+#    #+#             */
-/*   Updated: 2025/01/24 15:10:31 by sel-abbo         ###   ########.fr       */
+/*   Updated: 2025/01/25 09:58:45 by sel-abbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	signal_handler(int sig)
 	static int	bit_count;
 	static char	c;
 
-	bit_count = 0;
 	if (sig == SIGUSR1)
 		c |= (1 << (7 - bit_count));
 	bit_count++;
